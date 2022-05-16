18eede08b0dd2817672b4cb7a2e4d078ac152170
jhoemauricio <jhosephmauriciojhoseph@gmail.com git config --global user.name Your>
2022-05-16 17:57:44 -0300

    sair perfil usuario

---
+++ scripts/logOut.js
@@ -0,0 +1,40 @@
+
+function logOut(){
+
+    //Url 
+    var host = 'https://api.okei.online/sair';
+
+        $.ajax({
+
+            url: host,
+            type: 'GET',
+            xhrFields: {
+                //inclua cookies e cabeçalhos de autenticação 
+                withCredentials: true
+             },
+    
+    
+             success: function (){
+             
+             
+               // console.log('logoff');
+               top.location.href =  'login.html';
+             
+    
+             },
+    
+        }).fail(function(jqXHR, errorThrown){
+    
+            console.log(jqXHR.status);
+    
+           
+        });
+
+
+
+  
+}
+  
+
+
+   
--- scripts/logged.js
+++ scripts/logged.js
@@ -17,55 +17,7 @@ $(document).ready(function(){
    
         success: function(dados){
           
-
-              //converterá os dados em uma string antes de analisá-los
-              dados = JSON.stringify(dados);
-
-              dados = JSON.parse(dados);
-              // console.log(dados.roles);
-        
-              var array = dados.roles;
-
-              //percorre o array
-              Object.keys(array).forEach(function(key){
-
-              // console.log(array[key].nome);
-
-              switch(array[key].nome) {
-
-              
-                case "administrador":
-                  
-                  // console.log(dados);
-              
-                  nome = dados.nome;
-                  //dois parametos o separador e limite de divisões
-                  nome = nome.split(" ",2);
-
-                  //converte para string
-                  nomeNew = nome.toString();
-
-                  //procura por regex virgula, substitui pelo espaço
-                  nomeNew = nomeNew.replace(/,/g," ");
-
-                  //console.log(nomeNew);
-                  
-                  //chave e o valor a ser setado
-                  localStorage.setItem("tipoUser",array[key].nome);
-                  localStorage.setItem("nomePerfil",nomeNew);
-                  localStorage.setItem("nome", dados.nome);
-                  localStorage.setItem("email", dados.email);
-                  localStorage.setItem("nascimento", dados.data_nascimento);
-                  localStorage.setItem("nif", dados.nif);
-                  localStorage.setItem("pais", dados.pais);
-                  localStorage.setItem("sexo",dados.sexo);
-
-                  break;
-
-              }
-            
-          });  
-
+            console.log(dados);
             
         },
         
@@ -73,7 +25,13 @@ $(document).ready(function(){
         dataType: 'json',
 
     }).fail(function (jqXHR, errorThrown) {
-        console.log("ERROR");
-    
+
+        $erro = jqXHR.status;
+
+        if ($erro === 401) {
+             //console.log($erro);
+            top.location.href = 'login.html';
+      
+        }
     }
 )});
